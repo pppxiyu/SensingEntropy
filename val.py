6fcab66389f9aa5d2c01854b5d40e4bd54c49913
pppxiyu <xyp@gatech.edu>
2025-09-23 22:25:04 -0400

    working val (dis. + int.)

--- val.py
+++ val.py
@@ -40,9 +40,7 @@ bayes_network_normal.fit_marginal_from_data()
 
 # validate on each incident
 kld = []
-for i in road_data.flood_time_citywide.copy().drop(
-        road_data.flood_time_citywide.copy().sample(frac=0, random_state=df_random_seed).index
-).index:
+for i in road_data.flood_time_citywide.copy().index:
 
     # get inundated roads during the incident
     rd_test = dd.RoadData()  # empty instance
@@ -53,7 +51,7 @@ for i in road_data.flood_time_citywide.copy().drop(
 
     # filter inundations: bayes_network_t.marginals_downward[k] used
     if len([k for k in inundation if k in bayes_network_t.marginals_downward]) != len(inundation):
-        continue
+        continue  # flood time was determined using all data not filtered by coor, 150+ incidents were skipped
 
     # get traffic data during the incident
     d = rd_test.pull_nyc_dot_traffic_flooding(
@@ -61,21 +59,17 @@ for i in road_data.flood_time_citywide.copy().drop(
         select_incidents=road_data.flood_time_citywide.copy().iloc[[i]]
     )
     if d is None:
-        continue
+        continue  # the scope of modeling is based on aggregated data, data may be missing for some incidents
     rd_test.resample_nyc_dot_traffic(rd_test.speed)
 
     # fit marginals from the incident traffic data
     bn_t_test = mo.TrafficBayesNetwork(
         speed=rd_test.speed_resampled, road_geo=road_data.geo,
         n_samples=10000, n_components=12,
-        remove_nodes=remove_data_from_nodes, corr_thr=corr_thr,
+        remove_nodes=remove_data_from_nodes,
     )
     bn_t_test.fit_marginal_from_data()
 
-    # filter inundations: bn_t_test.marginals[k] used
-    if len([k for k in inundation if k in bn_t_test.marginals]) != len(inundation):
-        continue
-    
     # estimate using the trained BN
     marginals_flood, update_loc_f = bayes_network_t.update_network_with_multiple_soft_evidence(
         [{**{k: bn_t_test.marginals[k] for k in inundation}},  # signal_down
@@ -91,39 +85,40 @@ for i in road_data.flood_time_citywide.copy().drop(
     update_locs = [i for i in (update_loc_f['down'] + update_loc_f['up']) if i not in inundation]  # remove signal locs
     print(f'All traversed locs: {update_locs}')
 
-    # # compared disruption level 
-    # true_disruption = bayes_network_t.calculate_network_kl_divergence([
-    #     {k: v for k, v in bn_t_test.marginals.items() if k in update_locs},  # incident 
-    #     {k: v for k, v in bayes_network_normal.marginals.items() if k in update_locs},  # averaged (normal period)
-    # ], expand='True')
-    # estimated_disruption = bayes_network_t.calculate_network_kl_divergence([
-    #     {k: v for k, v in marginals_flood.items() if k in update_locs},  # estimate
-    #     {k: v for k, v in bayes_network_normal.marginals.items() if k in update_locs},  # averaged (normal period)
-    # ], expand='True')
-    # kld.append([true_disruption, estimated_disruption, update_locs])
-
-    # compared estimation error reduction (with flood belief)
-    error_prior = bayes_network_t.calculate_network_kl_divergence([
-        {k: v for k, v in bayes_network_t.marginals_downward.items() if k in update_locs},  # averaged (flood period)
+    # compared disruption level 
+    true_disruption = bayes_network_t.calculate_network_kl_divergence([
         {k: v for k, v in bn_t_test.marginals.items() if k in update_locs},  # incident 
-    ])
-    error_posterior = bayes_network_t.calculate_network_kl_divergence([
+        {k: v for k, v in bayes_network_normal.marginals.items() if k in update_locs},  # averaged (normal period)
+    ], expand='True'
+    )
+    estimated_disruption = bayes_network_t.calculate_network_kl_divergence([
         {k: v for k, v in marginals_flood.items() if k in update_locs},  # estimate
-        {k: v for k, v in bn_t_test.marginals.items() if k in update_locs},  # incident
-    ])
-    kld.append([error_prior, error_posterior, update_locs])
+        {k: v for k, v in bayes_network_normal.marginals.items() if k in update_locs},  # averaged (normal period)
+    ], expand='True'
+    )
+    kld.append([true_disruption, estimated_disruption, update_locs])
 
-# kld =  [
-#     [v1, v2, k[2]] for k in kld if (k[0] is not None and k[1] is not None) 
-#     for v1, v2 in zip(k[0], k[1]) if v1 is not None and v2 is not None
-# ]
-# relative_changes = [(i[1] - i[0]) / i[0] for i in kld]
-# print(f'Averaged relative change is: {sum(relative_changes) / len(relative_changes)}')
-# vis.scatter_diff_vs_estimated_diff([i[0] / len(i[2]) for i in kld], [i[1] / len(i[2]) for i in kld])
+    # # compared estimation error reduction (with flood belief)
+    # error_prior = bayes_network_t.calculate_network_kl_divergence([
+    #     {k: v for k, v in bayes_network_t.marginals_downward.items() if k in update_locs},  # averaged (flood period)
+    #     {k: v for k, v in bn_t_test.marginals.items() if k in update_locs},  # incident 
+    # ], expand='True'
+    # )
+    # error_posterior = bayes_network_t.calculate_network_kl_divergence([
+    #     {k: v for k, v in marginals_flood.items() if k in update_locs},  # estimate
+    #     {k: v for k, v in bn_t_test.marginals.items() if k in update_locs},  # incident
+    # ], expand='True'
+    # )
+    # kld.append([error_prior, error_posterior, update_locs])
 
-kld = [i for i in kld if i[0] is not None and i[1] is not None]
-relative_changes = [(i[1] - i[0]) / i[0] for i in kld]
+kld_expand =  [
+    [v1, v2, k[2]] for k in kld if (k[0] is not None and k[1] is not None) 
+    for v1, v2 in zip(k[0], k[1]) if v1 is not None and v2 is not None
+]  # no common keys when calculating KL divergence, or no propagated nodes
+relative_changes = [(i[1] - i[0]) / i[0] for i in kld_expand]
 print(f'Averaged relative change is: {sum(relative_changes) / len(relative_changes)}')
-vis.scatter_diff_vs_estimated_diff([i[0] / len(i[2]) for i in kld], [i[1] / len(i[2]) for i in kld])
+vis.scatter_diff_vs_estimated_diff(
+    [i[0] / len(i[2]) for i in kld_expand], [i[1] / len(i[2]) for i in kld_expand], xscale='linear', yscale='linear'
+)
 
 print('End of program.')
